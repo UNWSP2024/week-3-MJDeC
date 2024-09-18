a14f97ab796486e3ffa4b4b936bbf0b5fee68a28
MJDeC <mjdecaro@students.unwsp.edu>
2024-09-17 20:08:30 -0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -9,11 +9,11 @@ def categorize_age(age):
     ageCategory = "TBD"
     ######################
     # WRITE YOUR CODE HERE
-    if age<1:
+    if age<=1:
         print("infant")
     elif age>1 and age <13:
         print("child")
-    elif age>13 and age <20:
+    elif age>=13 and age <20:
         print("teenager")
     else:
         print("adult")
