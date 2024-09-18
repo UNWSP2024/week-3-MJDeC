1d6bedf20279a0060904e4e7140a3251a38ebbef
MJDeC <mjdecaro@students.unwsp.edu>
2024-09-17 20:03:48 -0500

    Update program_3.py

--- program_3.py
+++ program_3.py
@@ -14,6 +14,14 @@ def weight_conversion(weight):
     shippingCost = 0.0
     ######################
     # WRITE YOUR CODE HERE
+    if weight<2:
+       shippingCost=1.50
+    elif weight>2 and weight<6:
+        shippingCost=3.00
+    elif weight<6 and weight>10:
+       shippingCost=4.00
+    else:
+       shippingCost=4.75
     ######################
     
     return shippingCost
